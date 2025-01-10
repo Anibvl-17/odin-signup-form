904555e508947ae25aeae8d36888dcbf2e637ce1
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-09 23:49:54 -0300

    Add phone number validation with custom pattern message

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ confirmPasswordInput.addEventListener('input', function() {
     confirmPasswordInput.setCustomValidity('Passwords must match');
   }
 });
+
+const phoneInput = document.getElementById('phone');
+phoneInput.addEventListener('input', function() {
+  if (phoneInput.validity.patternMismatch) {
+    phoneInput.setCustomValidity('Expected the pattern 123-456-7890');
+  } else {
+    phoneInput.setCustomValidity('');
+  }
+});
