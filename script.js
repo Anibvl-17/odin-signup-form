37564f8ef885420baed8de7dff5c9699cab460f8
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-09 23:51:34 -0300

    Add name and last name validation with custom message

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ phoneInput.addEventListener('input', function() {
     phoneInput.setCustomValidity('');
   }
 });
+
+const nameInput = document.getElementById('name');
+nameInput.addEventListener('input', function() {
+  if (nameInput.validity.tooShort) {
+    nameInput.setCustomValidity('Name must be at least 2 characters long');
+  } else if (nameInput.validity.tooLong) {
+    nameInput.setCustomValidity('Name must be at most 30 characters long');
+  } else {
+    nameInput.setCustomValidity('');
+  }
+});
+
+const lastNameInput = document.getElementById('last-name');
+lastNameInput.addEventListener('input', function() {
+  if (lastNameInput.validity.tooShort) {
+    lastNameInput.setCustomValidity('Last name must be at least 2 characters long');
+  } else if (lastNameInput.validity.tooLong) {
+    lastNameInput.setCustomValidity('Last name must be at most 30 characters long');
+  } else {
+    lastNameInput.setCustomValidity('');
+  }
+});
